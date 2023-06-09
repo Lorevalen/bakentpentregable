56a472c29f722ca21da7acc6637b6c7b305b9e74
Lorevalen <127311609+Lorevalen@users.noreply.github.com>
2023-06-09 18:57:06 -0300

    Add files via upload

---
+++ bakendusuario.js
@@ -0,0 +1,65 @@
+{
+    "personas": [
+      {
+        "id": 1,
+        "nombre": "Juan Pérez",
+        "mail": "juan@example.com",
+        "telefono": "1234567890"
+      },
+      {
+        "id": 2,
+        "nombre": "María López",
+        "mail": "maria@example.com",
+        "telefono": "0987654321"
+      },
+      {
+        "id": 3,
+        "nombre": "Pedro Gómez",
+        "mail": "pedro@example.com",
+        "telefono": "5678901234"
+      },
+      {
+        "id": 4,
+        "nombre": "Ana Torres",
+        "mail": "ana@example.com",
+        "telefono": "9876543210"
+      },
+      {
+        "id": 5,
+        "nombre": "Carlos Rodríguez",
+        "mail": "carlos@example.com",
+        "telefono": "5432167890"
+      },
+      {
+        "id": 6,
+        "nombre": "Laura García",
+        "mail": "laura@example.com",
+        "telefono": "0123456789"
+      },
+      {
+        "id": 7,
+        "nombre": "Andrés Castro",
+        "mail": "andres@example.com",
+        "telefono": "6789012345"
+      },
+      {
+        "id": 8,
+        "nombre": "Isabel Ramírez",
+        "mail": "isabel@example.com",
+        "telefono": "2345678901"
+      },
+      {
+        "id": 9,
+        "nombre": "Miguel Sánchez",
+        "mail": "miguel@example.com",
+        "telefono": "8901234567"
+      },
+      {
+        "id": 10,
+        "nombre": "Sofía Vargas",
+        "mail": "sofia@example.com",
+        "telefono": "4567890123"
+      }
+    ]
+  }
+  
